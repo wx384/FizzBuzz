81d70ddf561c9d33f289f1634a30b888985513ac
Wei <evax384@gmail.com>
2025-09-09 18:27:07 -0400

    created Reduce class

---
+++ test/lab1/Reduce.java
@@ -0,0 +1,20 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduce(100));
+    }
+
+    public static int reduce(int n) {
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n - 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
