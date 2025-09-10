ace3d08567eaa5aa9aa243a673032693ebea5293
Wei <evax384@gmail.com>
2025-09-09 21:09:52 -0400

    optimized multiples by finding the number of multiples for each num and substract any double counting by lcm.

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,21 +2,33 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(multiples(1000, 5, 5));
+        System.out.println(multiples(1000, 5, 3));
     }
 
     public static int multiples(int n, int a, int b) {
+//        int count = 0;
+//        for (int i = 1; i < n; i++) {
+//            ;
+//            boolean multiplesOfA = i % a == 0;
+//            boolean multiplesOfB = i % b == 0;
+//
+//            if (multiplesOfA || multiplesOfB) {
+//                count++;
+//            }
+//        }
+//        return count;
         int count = 0;
-        for (int i = 1; i < n; i++) {
-            ;
-            boolean multiplesOfA = i % a == 0;
-            boolean multiplesOfB = i % b == 0;
-
-            if (multiplesOfA || multiplesOfB) {
-                count++;
-            }
+        if (a == b){
+            count = (n - 1) / a;
+        } else {
+            count = (n - 1) / a + (n - 1) / b - (n - 1) / ((a * b) / gcd(a, b));
         }
         return count;
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 
     public static int multiples() {
